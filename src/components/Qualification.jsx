94a598fdca2823607d382ffa9abd6ce95bda9823
Daniel Kim <danielkim2711@gmail.com>
2022-05-19 03:54:13 +1200

    Add qualification tabs

--- src/components/Qualification.jsx
+++ src/components/Qualification.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { FaGraduationCap } from 'react-icons/fa';
+import { BsFillBriefcaseFill } from 'react-icons/bs';
 
 const Qualification = () => {
   return (
@@ -6,9 +7,32 @@ const Qualification = () => {
       <h2 className='text-2xl font-bold' id='section__title'>
         Qualification
       </h2>
-      <span className='block text-sm mb-12 text-[#999]' id='section__subtitle'>
+      <span className='block text-sm text-[#999]' id='section__subtitle'>
         My personal journey
       </span>
+
+      <div id='qualification__container container'>
+        <div
+          className='flex justify-center items-center mt-20'
+          id='qualification__tabs'
+        >
+          <div
+            className='flex items-center mx-6'
+            id='qualification__button button--flex'
+          >
+            <FaGraduationCap className='w-7 h-7 mr-2' />
+            <p className='text-xl'>Education</p>
+          </div>
+
+          <div
+            className='flex items-center mx-6'
+            id='qualification__button button--flex'
+          >
+            <BsFillBriefcaseFill className='w-5 h-5 mr-2' />
+            <p className='text-xl'>Work</p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
