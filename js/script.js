ba7e3100c7f248c841a834ec7a68f83a0607fc12
Daniel Kim <danielkim2711@gmail.com>
2022-06-17 22:19:46 +1200

    Add animation to about items

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-const headerTitle = document.querySelector('.header__title');
-const headerText = document.querySelector('.header__text');
-
 let i = 0;
 let j = 0;
 
 let isFinished = false;
 
 function typeWriter() {
+  const headerTitle = document.querySelector('.header__title');
+  const headerText = document.querySelector('.header__text');
+
   const title = 'Daniel Kim';
   const text = 'Front-end developer';
   const speed = 60;
@@ -35,3 +35,22 @@ function typeWriter() {
 }
 
 typeWriter();
+
+// Add animation class to about items when scroll
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const polaroid = entry.target.querySelector('.polaroid');
+    const card = entry.target.querySelector('.card');
+
+    if (entry.isIntersecting) {
+      polaroid.classList.add('polaroid-animation');
+      card.classList.add('card-animation');
+      return;
+    }
+
+    polaroid.classList.remove('polaroid-animation');
+    card.classList.remove('card-animation');
+  });
+});
+
+observer.observe(document.querySelector('.about__items'));
