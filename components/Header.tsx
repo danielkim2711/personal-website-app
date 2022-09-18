9530f8e1519bcfd1401db3d2f803d800e1a1e31f
Daniel Kim <danielkim2711@gmail.com>
2022-09-18 22:35:26 +1200

    Install and implement framer motion

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,24 +1,64 @@
 import { SocialIcon } from 'react-social-icons';
+import { motion } from 'framer-motion';
 
 type Props = {};
 
 const Header = (props: Props) => {
   return (
     <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
-      <div className='flex flex-row items-center'>
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1,
+        }}
+        className='flex flex-row items-center'
+      >
         <SocialIcon
           url='https://github.com/danielkim2711'
           fgColor='gray'
           bgColor='transparent'
+          target='_blank'
         />
         <SocialIcon
           url='https://www.linkedin.com/in/danielkim2711'
           fgColor='gray'
           bgColor='transparent'
+          target='_blank'
         />
-      </div>
+        <SocialIcon
+          url='https://danielpersonalblog.herokuapp.com'
+          fgColor='gray'
+          bgColor='transparent'
+          target='_blank'
+          label='My personal blog'
+        />
+      </motion.div>
 
-      <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1,
+        }}
+        className='flex flex-row items-center text-gray-300 cursor-pointer'
+      >
         <SocialIcon
           className='cursor-pointer'
           network='email'
@@ -28,7 +68,7 @@ const Header = (props: Props) => {
         <p className='uppercase hidden text-sm text-gray-400 md:inline-flex'>
           Get in touch
         </p>
-      </div>
+      </motion.div>
     </header>
   );
 };
