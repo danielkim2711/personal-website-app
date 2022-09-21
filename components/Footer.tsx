7a9e9fa373303e6ada3e3ef31cfe32f16a260c91
Daniel Kim <danielkim2711@gmail.com>
2022-09-22 11:24:19 +1200

    Change cursor pointer from footer to footer Image

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,14 +6,14 @@ type Props = {};
 const Footer = (props: Props) => {
   return (
     <Link href='#hero'>
-      <footer className='sticky bottom-5 w-full cursor-pointer z-20'>
+      <footer className='sticky bottom-5 w-full z-20'>
         <div className='flex justify-center items-center'>
           <Image
             src='/static/about-image.jpeg'
             alt='my portrait'
             width={40}
             height={40}
-            className='rounded-full object-cover filter grayscale hover:grayscale-0'
+            className='rounded-full object-cover cursor-pointer filter grayscale hover:grayscale-0'
           />
         </div>
       </footer>
