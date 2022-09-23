060128da98260ca038cb9dd7f6bd72372a389b42
Daniel Kim <danielkim2711@gmail.com>
2022-09-23 15:55:20 +1200

    Make only image clickable

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,19 +5,21 @@ type Props = {};
 
 const Footer = (props: Props) => {
   return (
-    <Link href='#hero'>
-      <footer className='sticky bottom-5 w-full z-20'>
-        <div className='flex justify-center items-center'>
-          <Image
-            src='/static/about-image.jpeg'
-            alt='my portrait'
-            width={40}
-            height={40}
-            className='rounded-full object-cover cursor-pointer filter grayscale hover:grayscale-0'
-          />
-        </div>
-      </footer>
-    </Link>
+    <footer className='sticky bottom-5 w-full z-20'>
+      <div className='flex justify-center items-center'>
+        <Link href='#hero'>
+          <span>
+            <Image
+              src='/static/about-image.jpeg'
+              alt='my portrait'
+              width={40}
+              height={40}
+              className='rounded-full object-cover cursor-pointer filter grayscale hover:grayscale-0'
+            />
+          </span>
+        </Link>
+      </div>
+    </footer>
   );
 };
 
