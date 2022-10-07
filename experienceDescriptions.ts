3f3342035a5396da2d52a99062b1e2f419eb4b6f
Daniel Kim <danielkim2711@gmail.com>
2022-10-07 22:22:24 +1300

    Update experience descriptions

--- experienceDescriptions.ts
+++ experienceDescriptions.ts
@@ -1,19 +1,19 @@
 export const jetCleaningDescriptions = [
   'Managing the daily, weekly and monthly cleaning for clients',
   'Communicating with clients & franchisor',
-  'Be always on call for any customer complaints or future events',
-  'Hiring staff and organising the team',
+  'Always be on call for any customer complaints or future events',
+  'Hiring staff and organising them',
   'Meeting with clients for a weekly review',
   'Inspecting the quality of cleaning for clients',
-  'Organising cleaning equipments',
+  'Maintaining cleaning equipment',
 ];
 
 export const unitecDescriptions = [
   'Successfully delivered a full-stack web application to a charity in the Capstone Project',
   'The aim was to focus on data management to organise their existing data into a centralised database',
-  'React.js and TypeScript for the front-end',
-  'ASP.NET and C# for the back-end',
-  'PostgreSQL for the database',
+  'React.js and TypeScript (Front End)',
+  'ASP.NET and C# (Back End)',
+  'PostgreSQL (Database)',
   'AWS Elastic Beanstalk for hosting',
   'Agile methodology with Kanban framework',
   'Awarded the Top Software Project',
